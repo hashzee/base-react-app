321bf926257d129ca3042f5baec23153eba27931
hashzee <55513359+hashzee@users.noreply.github.com>
2020-06-02 12:43:54 +0500

    Base App Setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="overlay">  
+      <div className="App">
+        <header className="App-header">         
+            <div className="fix">
+              <h1 className='Greeting'>Welcome Aboard</h1>
+            </div>
+            <h2>Ready to take off with React </h2>
+            <h5>Assignment 2: Zeeshan Hashmi</h5>         
+        </header>
+        <div className="App-logo">
+            <img src={logo} />
+        </div>          
+      </div>
+    </div>
+  );
+}
+
+export default App;
